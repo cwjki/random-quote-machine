6b022c3554cb47afa58ea34c8b7b9249907bb3bb
Juan Carlos Casteleiro Wong <cwjki@MacBookcita-de-Jki.local>
2023-03-09 17:28:18 -0500

    adding some quotes

---
+++ src/data/quotes.js
@@ -0,0 +1,104 @@
+const quotes = [
+    {
+        id: 1,
+        quote: "Success is not final, failure is not fatal: It is the courage to continue that counts.",
+        author: "Winston Churchill",
+    },
+    {
+        id: 2,
+        quote: "If you can dream it, you can do it.",
+        author: "Walt Disney",
+    },
+    {
+        id: 3,
+        quote: "Your time is limited, don't waste it living someone else's life.",
+        author: "Steve Jobs",
+    },
+    {
+        id: 4,
+        quote: "If you want something you've never had, you must be willing to do something you've never done.",
+        author: "Unknown",
+    },
+    {
+        id: 5,
+        quote: "Success is stumbling from failure to failure with no loss of enthusiasm.",
+        author: "Winston Churchill",
+    },
+    {
+        id: 6,
+        quote: "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.",
+        author: "Albert Einstein",
+    },
+    {
+        id: 7,
+        quote: "A man who dares to waste one hour of time has not discovered the value of life.",
+        author: "Charles Darwin",
+    },
+    {
+        id: 8,
+        quote: "You cannot teach a man anything; you can only help him discover it in himself.",
+        author: "Galileo Galilei",
+    },
+    {
+        id: 9,
+        quote: "Imagination is more important than knowledge.",
+        author: "Albert Einstein",
+    },
+    {
+        id: 10,
+        quote: "",
+        author: "",
+    },
+    {
+        id: 11,
+        quote: "",
+        author: "",
+    },
+    {
+        id: 12,
+        quote: "",
+        author: "",
+    },
+    {
+        id: 13,
+        quote: "",
+        author: "",
+    },
+    {
+        id: 14,
+        quote: "",
+        author: "",
+    },
+    {
+        id: 15,
+        quote: "",
+        author: "",
+    },
+    {
+        id: 16,
+        quote: "",
+        author: "",
+    },
+    {
+        id: 17,
+        quote: "",
+        author: "",
+    },
+    {
+        id: 18,
+        quote: "",
+        author: "",
+    },
+    {
+        id: 19,
+        quote: "",
+        author: "",
+    },
+    {
+        id: 20,
+        quote: "",
+        author: "",
+    },
+]
+
+export default quotes;
