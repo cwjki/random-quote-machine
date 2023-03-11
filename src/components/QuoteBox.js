1746ddf7c04dff22babce8ff69a470e4888a027b
cwjki <cwjki97@gmail.com>
2023-03-11 16:05:47 -0500

    random quote generator working

--- src/components/QuoteBox.js
+++ src/components/QuoteBox.js
@@ -3,10 +3,14 @@ import quotes from "../data/quotes";
 
 
 const QuoteBox = () => {
-  const [index, setIndex] = useState()
+  const [index, setIndex] = useState(0);
 
   const onClick = () => {
     let rIndex = Math.floor(Math.random() * quotes.length);
+    while (rIndex === index) {
+      rIndex = Math.floor(Math.random() * quotes.length);
+    }
+    setIndex(rIndex);
   }
 
   return (
@@ -15,8 +19,8 @@ const QuoteBox = () => {
         <div className="col-6 mx-auto">
           <div className="card shadow rounded-3">
             <div className="card-body d-flex flex-column">
-                <p className="fs-3 text-center fw-semibold">"{quotes[0].quote}"</p>
-                <p className="fs-5 text-center fw-light">{quotes[0].author}</p>
+                <p className="fs-3 text-center fw-semibold">"{quotes[index].quote}"</p>
+                <p className="fs-5 text-center fw-light">{quotes[index].author}</p>
                 <div className="row">
                   <div className="col-6">
                     <a href="twitter">
@@ -27,7 +31,7 @@ const QuoteBox = () => {
                     </a>
                   </div>
                   <div className="col-6 text-end">
-                    <button className="btn btn-lg btn-outline-primary rounded-4" onClick={this.onClick}>New Quote</button>
+                    <button className="btn btn-lg btn-outline-primary rounded-4" onClick={onClick}>New Quote</button>
                   </div>
                 </div>
             </div>
@@ -38,24 +42,5 @@ const QuoteBox = () => {
     </div>
   )
 }
-
-// class QuoteBox1 extends React.Component{
-//   constructor(props) {
-//     super(props)
-//     this.state = {
-//       rIndex: 0
-//     };
-//     this.onClick = this.onClick.bind(this);
-//   }
-
-
-  
-
-//   render() {
-    
-//   }
-// }
-
-
 
 export default QuoteBox;
