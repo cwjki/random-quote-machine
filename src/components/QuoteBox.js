6fc712442853559ab747884795726311f68500bc
cwjki <cwjki97@gmail.com>
2023-03-11 17:25:34 -0500

    changing colors randomly

--- src/components/QuoteBox.js
+++ src/components/QuoteBox.js
@@ -21,21 +21,21 @@ const QuoteBox = () => {
   }
 
   return (
-    <div className="container-fluid bg-primary vw-100 vh-100" id="quote-box">
+    <div className="container-fluid vw-100 vh-100" id="quote-box" style={{backgroundColor: colors[colorIndex]}}>
       <div className="row align-items-center vh-100">
         <div className="col-6 mx-auto">
           <div className="card shadow rounded-3">
             <div className="card-body d-flex flex-column">
-                <p id="text" className="fs-3 text-center fw-semibold"><i className="bi bi-quote" style={{fontSize: 50}}></i>{quotes[quoteIndex].quote}</p>
-                <p id="author" className="fs-5 text-center fw-light">- {quotes[quoteIndex].author}</p>
+                <p style={{color: colors[colorIndex]}} id="text" className="fs-3 text-center fw-semibold"><i className="bi bi-quote" style={{fontSize: 50}}></i>{quotes[quoteIndex].quote}</p>
+                <p style={{color: colors[colorIndex]}} id="author" className="fs-5 text-center fw-light">- {quotes[quoteIndex].author}</p>
                 <div className="row">
                   <div className="col-6">
-                    <a id="tweet-quote" href="https://twitter.com/intent/tweet" target="_blank" rel="noreferrer">
-                      <button className="btn btn-lg btn-outline-primary rounded-3"><i class="bi bi-twitter"></i></button>
+                    <a style={{color: colors[colorIndex]}} id="tweet-quote" href="https://twitter.com/intent/tweet" target="_blank" rel="noreferrer">
+                      <button className="btn btn-lg rounded-3"><i class="bi bi-twitter"></i></button>
                     </a>
                   </div>
                   <div className="col-6 text-end">
-                    <button id="new-quote" className="btn btn-lg btn-outline-primary rounded-3" onClick={onClick}>New Quote</button>
+                    <button style={{color: colors[colorIndex]}} id="new-quote" className="btn btn-lg rounded-3" onClick={onClick}>New Quote</button>
                   </div>
                 </div>
             </div>
